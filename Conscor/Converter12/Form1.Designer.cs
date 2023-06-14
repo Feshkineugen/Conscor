c4427664290a9dcc559622d85bf0df923b055589
Feshkineugen <118047190+Feshkineugen@users.noreply.github.com>
2023-06-14 23:36:08 +0300

    Add files via upload

---
+++ Conscor/Converter12/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Converter12
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton3 = new RadioButton();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Yu Gothic UI", 27.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(267, 44);
+            label1.Name = "label1";
+            label1.Size = new Size(396, 50);
+            label1.TabIndex = 0;
+            label1.Text = "Конвертер скоростей";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Yu Gothic UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(121, 128);
+            label2.Name = "label2";
+            label2.Size = new Size(152, 21);
+            label2.TabIndex = 1;
+            label2.Text = "Входное значение : ";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Yu Gothic UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(527, 128);
+            label3.Name = "label3";
+            label3.Size = new Size(87, 21);
+            label3.TabIndex = 2;
+            label3.Text = "Результат :";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Yu Gothic UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(121, 212);
+            label4.Name = "label4";
+            label4.Size = new Size(157, 21);
+            label4.TabIndex = 3;
+            label4.Text = "Выберите единицу : ";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(127, 171);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(146, 23);
+            textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(527, 171);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(146, 23);
+            textBox2.TabIndex = 5;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(128, 249);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(223, 19);
+            radioButton1.TabIndex = 6;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Километры в час в метры в секунду";
+            radioButton1.UseVisualStyleBackColor = true;
+            radioButton1.CheckedChanged += radioButton1_CheckedChanged;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(127, 274);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(220, 19);
+            radioButton2.TabIndex = 7;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Километры в час в метры в минуту";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(127, 299);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(128, 19);
+            radioButton3.TabIndex = 8;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "Узлы в километры";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(372, 347);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 49);
+            button1.TabIndex = 9;
+            button1.Text = "Перевод";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Location = new Point(272, 347);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 49);
+            button2.TabIndex = 10;
+            button2.Text = "Сброс";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.Location = new Point(472, 347);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 49);
+            button3.TabIndex = 11;
+            button3.Text = "Выход";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.MenuHighlight;
+            ClientSize = new Size(799, 485);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(radioButton3);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            ForeColor = SystemColors.ActiveCaptionText;
+            Name = "Form1";
+            Text = "Converter";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton3;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+    }
+}
---
+++ Conscor/Converter12/Form1.cs
@@ -0,0 +1,60 @@
+namespace Converter12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a, b;
+
+            if (radioButton1.Checked == true)
+            {
+                a = Double.Parse(textBox1.Text);
+                b = a * 1000 / 3600;
+                textBox2.Text = Convert.ToString(b);
+            }
+            if (radioButton2.Checked == true)
+            {
+                a = Double.Parse(textBox1.Text);
+                b = a * 1000 / 60;
+                textBox2.Text = Convert.ToString(b);
+            }
+            if (radioButton3.Checked == true)
+            {
+                a = Double.Parse(textBox1.Text);
+                b = a * 1.852;
+                textBox2.Text = Convert.ToString(b);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
